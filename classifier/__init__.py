06bab6e373bdcca4b8cb4de85be74381b83d8488
paperClub-hub <qhs2018@sina.com>
2022-09-12 11:17:41 +0800

    init

---
+++ classifier/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__, static_folder='static', template_folder='templates',instance_relative_config=True)
+app.config.from_object('classifier.default_settings')
+app.config.from_pyfile('application.cfg', silent=True)
+db = SQLAlchemy(app)
+#print(db)
+from classifier import routes
